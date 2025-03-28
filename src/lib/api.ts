538768594232442465d328a00cf50f4f122f522c
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2025-03-28 21:36:48 +0100

    use render api sserver

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,2 @@
-// export const API_URL = import.meta.env.VITE_API_URL || "https://ringer-lity.onrender.com";
-export const API_URL = import.meta.env.VITE_API_URL || "https://0144-102-89-22-207.ngrok-free.app"
+export const API_URL = import.meta.env.VITE_API_URL || "https://ringer-lity.onrender.com";
+// export const API_URL = import.meta.env.VITE_API_URL || "https://0144-102-89-22-207.ngrok-free.app"
